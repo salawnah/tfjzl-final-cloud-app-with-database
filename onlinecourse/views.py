afbf0d8daba20506d6f614c5e5fd30424cce10bd
Sameer Alawnah <sameer@alawnah.com>
2023-12-26 12:37:59 -0500

    task4 done

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 # <HINT> Import any new Models here
-from .models import Course, Enrollment
+from .models import Course, Enrollment, Question, Choice, Submission
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
 from django.urls import reverse
@@ -111,7 +111,8 @@ def enroll(request, course_id):
          # Add each selected choice object to the submission object
          # Redirect to show_exam_result with the submission id
 #def submit(request, course_id):
-
+def submit(request, course_id):
+    course = Course.objects.get(pk=course_id)
 
 # An example method to collect the selected choices from the exam form from the request object
 def extract_answers(request):
